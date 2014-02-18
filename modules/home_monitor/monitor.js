3ff5d8489680a4fa473c8557a0bf3c7a9a16789f
Loren West <email@lorenwest.com>
2014-02-17 18:33:12 -0800

    Added home_monitor

---
+++ modules/home_monitor/monitor.js
@@ -0,0 +1,20 @@
+// monitor.js (c) 2010-2014 Loren West and other contributors
+// May be freely distributed under the MIT license.
+// For further details and documentation:
+// http://github.com/lorenwest/monitor-dashboard
+(function(root){
+
+  // Load dependencies
+  var Dashboard = require('monitor-dashboard');
+
+  // Display the logo
+  console.log("");
+  console.log("             _________                                  __________");
+  console.log("___________________  /____      _______ ___________________(_)_  /______________");
+  console.log("__  __ \\  __ \\  __  /_  _ \\     __  __ `__ \\  __ \\_  __ \\_  /_  __/  __ \\_  ___/");
+  console.log("_  / / / /_/ / /_/ / /  __/     _  / / / / / /_/ /  / / /  / / /_ / /_/ /  /");
+  console.log("/_/ /_/\\____/\\__,_/  \\___/      /_/ /_/ /_/\\____//_/ /_//_/  \\__/ \\____//_/");
+  console.log("");
+
+
+}(this));
